18c2a750cde5c3ca2a310f8e22fa00a781e6a0aa
yukl-c <yulkchen@gmail.com>
2025-10-14 05:39:24 -0400

    Deploy sentiment analysis on Code Engine as a microservice

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,9 +16,16 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # path for get a review view
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
 
+
+        path(route='add_review', view=views.add_review, name='add_review'),
     # path for get car model list
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
