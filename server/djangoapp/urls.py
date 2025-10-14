4c6f03ea86e6b3c899fa1f74c19a67c22c0151d2
yukl-c <yulkchen@gmail.com>
2025-10-14 09:32:52 -0400

    Add dynamic pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for get a review view
