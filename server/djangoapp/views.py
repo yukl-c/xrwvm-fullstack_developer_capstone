5bf3ab4540de73e44ebc785c942aca0ebe8ce367
yukl-c <72858964+yukl-c@users.noreply.github.com>
2025-10-17 15:11:41 +0800

    Refactor authentication and registration views
    Refactor views.py for improved clarity and structure.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,26 +1,17 @@
-# Uncomment the required imports before adding the code
-
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
-
+from django.contrib.auth import logout, login, authenticate
 from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+from django.views.decorators.csrf import csrf_exempt
 import logging
 import json
-from django.views.decorators.csrf import csrf_exempt
+
 from .populate import initiate
-
 from .models import CarMake, CarModel
-
 from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 
@@ -31,11 +22,11 @@ def login_user(request):
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
+    # Try to check if provided credentials can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
     if user is not None:
-        # If user is valid, call login method to login current user
+        # If user is valid, log in the current user
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
@@ -51,8 +42,6 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
-
     # Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
@@ -61,14 +50,14 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
     except Exception:
-        # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
+        # If not, simply log that this is a new user
+        logger.debug("%s is a new user", username)
 
     # If it is a new user
     if not username_exist:
@@ -80,19 +69,18 @@ def registration(request):
             password=password,
             email=email,
         )
-        # Login the user and redirect to list page
+        # Login the user and return status
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else:
-        data = {"userName": username, "error": "Already Registered"}
-        return JsonResponse(data)
+
+    data = {"userName": username, "error": "Already Registered"}
+    return JsonResponse(data)
 
 
 # Create `get_cars` view to render the car model list
 def get_cars(request):
     count = CarMake.objects.filter().count()
-    print(count)
     if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
@@ -104,8 +92,8 @@ def get_cars(request):
     return JsonResponse({"CarModels": cars})
 
 
-# Update the `get_dealerships` render list of dealerships all by default,
-# particular state if state is passed
+# Update the `get_dealerships` to render list of dealerships:
+# all by default, or a particular state if state is passed
 def get_dealerships(request, state="All"):
     if state == "All":
         endpoint = "/fetchDealers"
@@ -117,25 +105,23 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
-    # if dealer id has been provided
+    # If dealer id has been provided
     if dealer_id:
         try:
             endpoint = "/fetchReviews/dealer/" + str(dealer_id)
             reviews = get_request(endpoint)
             for review_detail in reviews:
                 response = analyze_review_sentiments(review_detail['review'])
-                print(response)
                 review_detail['sentiment'] = response.get(
                     'sentiment',
-                    'neutral'
+                    'neutral',
                 )
             return JsonResponse({"status": 200, "reviews": reviews})
         except Exception:
             return JsonResponse(
                 {"status": 500, "message": "Internal server error"}
             )
-    else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+    return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
 # Create a `get_dealer_details` view to render the dealer details
@@ -144,23 +130,18 @@ def get_dealer_details(request, dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
-    else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+    return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
 # Create an `add_review` view to submit a review
 def add_review(request):
-    if request.user.is_anonymous is False:
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            # response = post_review(data)
             post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse(
                 {"status": 401, "message": "Error in posting review"}
             )
-    else:
-        return JsonResponse(
-            {"status": 403, "message": "Unauthorized"}
-        )
+    return JsonResponse({"status": 403, "message": "Unauthorized"})
