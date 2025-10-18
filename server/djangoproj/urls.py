53e7a6e23f26993563f31595afbb71ce9d674b53
yukl-c <yulkchen@gmail.com>
2025-10-18 01:10:06 -0400

    containerize

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -29,11 +29,11 @@ urlpatterns = [
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path(
-        'dealer/int:dealer_id',
-        TemplateView.as_view(template_name="index.html"),
+        'dealer/int:dealer_id', 
+        TemplateView.as_view(template_name="index.html")
     ),
     path(
         'postreview/int:dealer_id',
-        TemplateView.as_view(template_name="index.html"),
+         TemplateView.as_view(template_name="index.html")
     ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
